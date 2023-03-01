884a43efbf36370d19b295b236e925953e070361
kleelof <kleelof@yahoo.com>
2023-03-01 07:31:33 -0800

    initial;

---
+++ server.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, flash
+
+from classes.joke import Joke
+Joke.load()
+
+app = Flask(__name__)
+app.secret_key = 'abc123!%&'
+
+
+@app.route('/')
+def display_home():
+    return render_template('home.html', joke = Joke.get_by_id(1))
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html', rows=Joke.get_all())
+
+@app.route('/joke/view/<int:id>')
+def view_joke(id):
+    return render_template('view.html', joke=Joke.get_by_id(id))
+
+@app.route('/joke/add')
+def get_add_joke_form():
+    return render_template('add.html')
+
+@app.route('/joke/add', methods=['POST'])
+def add_joke():
+    Joke.create(request.form['text'], request.form['punchline'])
+    flash('Your Joke has been added.')
+    return redirect('/dashboard')
+
+@app.errorhandler(404)
+def handle_404(e):
+    return "You're lost, get a map!"
+
+if __name__=="__main__":
+    app.run(debug=True)
